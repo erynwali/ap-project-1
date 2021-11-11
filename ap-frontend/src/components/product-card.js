37273a6094fa2df89035e1bf70db89bb7b4d56ff
Ammar Sura <ammarsura@Ammars-MacBook-Air.local>
2021-11-11 19:58:24 +0530

    change

--- ap-frontend/src/components/product-card.js
+++ ap-frontend/src/components/product-card.js
@@ -7,7 +7,6 @@ export default class ProductCardComp extends Component {
     render() {
       return (
        
-        // <h1>Hi</h1>
         <Card style={{width : "20%"}}>
             <Card.Img variant="top" src={this.props.src} />
             <Card.Body style={{height:"80px"}}>
