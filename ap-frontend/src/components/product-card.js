d0dd6228cb4501a9fde302e6697460a577b29042
Ammar Sura <ammarsura@Ammars-MacBook-Air.local>
2021-11-11 19:43:08 +0530

    added product card

---
+++ ap-frontend/src/components/product-card.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { CardGroup, Card,  } from "react-bootstrap";
+import "../App.css";
+
+
+export default class ProductCardComp extends Component {
+    render() {
+      return (
+        <div>
+
+            {/* <h1>{this.props.name}</h1> */}
+        
+        <Card style={{width : "20%"}}>
+            <Card.Img variant="top" src={this.props.src} />
+            <Card.Body style={{height:"80px"}}>
+            <Card.Title class="product-card-title">{this.props.title}</Card.Title>
+            <Card.Text class="product-card-text">
+                {this.props.description}
+            </Card.Text>
+            </Card.Body>
+        </Card>
+        </div>
+      )
+    } 
+} 
