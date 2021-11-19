8cfefd2e78cf3bfa40f661d6c60ed159b85a78ee
Eryn Wali <erynwali@hotmail.com>
2021-11-20 00:29:24 +0530

    Multiple changes

--- ap-frontend/src/pages/Product.js
+++ ap-frontend/src/pages/Product.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "../App.css";
 import {Link} from 'react-router-dom';
-import {Container, Row, Col, Image, Card, Button} from "react-bootstrap"
+import {Container, Row, Col, Image, Card, Button, DropdownButton, Dropdown, Nav} from "react-bootstrap"
 
 export default class Product extends Component {
 
@@ -10,7 +10,7 @@ export default class Product extends Component {
       <div> 
 
         <Container style={{
-            marginTop: "8%",
+            marginTop: "10%",
             marginRight: "7%"
         }}>
         <Row>
@@ -28,17 +28,49 @@ export default class Product extends Component {
 
         <div style={{
             justifyContent: "right", 
-            marginLeft: "7%"
+            marginLeft: "7%", 
+            marginRight: "68%"
         }}>
         
         <h1 style={{
-            marginTop:"5%"
+            marginTop:"10%"
         }}>Brown Sweatshirt</h1>
 
-         Heart </div>
+        <h3> Rs 1799 </h3>
 
-
+      Top in printed sweatshirt fabric made from a cotton blend. Relaxed fit with dropped shoulders, long sleeves and ribbing around the neckline, cuffs and hem. The polyester content of the sweatshirt is recycled.
        
+      <div style={{
+        marginTop: "3%", 
+        marginBottom: "5%"
+      }}> 
+
+      <Dropdown>
+        <Dropdown.Toggle id="dropdown-button-dark-example1" variant="light">
+          Select Size
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu variant="light">
+          <Dropdown.Item href="#/action-1"> XS </Dropdown.Item>
+          <Dropdown.Item href="#/action-2"> S </Dropdown.Item>
+          <Dropdown.Item href="#/action-3"> M </Dropdown.Item>
+          <Dropdown.Item href="#/action-3"> L </Dropdown.Item>
+          <Dropdown.Item href="#/action-3"> XL </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
+      <div className="d-grid gap-2" style={{
+        marginTop: "3%"
+      }}>
+      <Button variant="secondary" size="lg">
+      Add to Cart
+      </Button>
+      </div>
+
+      </div>
+
+      </div>
+
       </div>
     );
   }
